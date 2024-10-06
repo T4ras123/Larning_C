2909ed6f7a19743ead8f0ff4b88c6e604bc2a161
T4ras123 <vovatara123@gmail.com>
2024-10-06 19:16:50 +0400

    Update .gitignore and add variables.c example

---
+++ VeryBeginning/variables.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main() {
+
+    int num1;
+    num1 = 15;
+    float num2 = 1.5;
+    char ch = 'A';
+    int x = 1, y = 2, z = 3;
+    int X, Y, Z;
+    X = Y = Z  = 10;
+
+    printf("%d", num1);
+    printf("%f", num2);
+    printf("%c", ch);
+    return 0;
+
+}
